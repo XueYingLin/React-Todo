a498946bba3fd9b7eb23764398c74eb466fd3a04
Nikki Lin <nikkiylin@gmail.com>
2020-03-09 15:51:00 -0700

    add button is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+
+
+const tasks = [
+  {
+    task: "Organize Garage",
+    id: 101,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 102,
+    completed: false  
+  },
+  {
+    task: "Shop groceries",
+    id: 103,
+    completed: false
+  }
+  
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any change handlers you need to work with your 
+  
+  constructor() {
+    super();
+    this.state = {
+      tasks,
+      task: ""
+    }
+  }
+
+  togglePurchased = clickedItemId => {
+    this.setState({
+      groceries: this.state.tasks.map(item => {
+        if (item.id === clickedItemId) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    })
+  }
+
+  addTask = taskName => {
+    const newTask = {
+    name: taskName,
+    id: new Date(),
+    compleated: false
+    };
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div>
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTask={this.addTask} />
+        </div>
+        <TodoList
+          tasks={this.state.tasks}
+          toggleCompleted={this.toggleComplesed} />
       </div>
     );
   }
